43ba8e0da421d4a1b919b1a305fb0264f617b62a
ryanformio <ryan@form.io>
2023-08-18 13:50:31 -0500

    Init test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# This script will configure the runner and start it
 
-./actions-runner/config.sh --url $GITHUB_REPO_URL --token $GITHUB_RUNNER_TOKEN
-./actions-runner/run.sh
+# Register Runner
+curl -o actions-runner-linux-x64-2.307.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.307.1/actions-runner-linux-x64-2.307.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.307.1.tar.gz
+rm -rf ./actions-runner-linux-x64-2.307.1.tar.gz
+./config.sh --url $REPO_URL --token $RUNNER_TOKEN
+
+# Run
+./run.sh
